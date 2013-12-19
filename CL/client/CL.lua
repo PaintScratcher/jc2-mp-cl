bc55e646320773e6538946e28c937708f242332b
adam <hvfcrazy@hotmail.com>
2013-12-19 10:10:32 +0000

    Added client side

---
+++ CL/client/CL.lua
@@ -0,0 +1,19 @@
+class 'CL'
+
+function CL:__init()
+    Events:Subscribe( "ModuleLoad", self, self.ModulesLoad )
+    Events:Subscribe( "ModulesLoad", self, self.ModulesLoad )
+end
+
+function CL:ModulesLoad()
+    Events:FireRegisteredEvent( "HelpAddItem",
+        {
+            name = "CL",
+            text = 
+                "JC2-MP Module 'CL' by Chris Lewis \n\n" ..
+				"Source available at http://github.com/C-D-Lewis/jc2-mp-cl\n\n" ..
+				"/help for list of commands" 
+        } )
+end
+
+CL = CL()
